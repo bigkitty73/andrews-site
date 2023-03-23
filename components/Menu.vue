ab5b67ad08dff028176290a5f0aeebe4c1202d70
Andrew Marple <andrewmarple03@gmail.com>
2023-03-23 16:43:02 -0400

    wip

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="top-bar">
       <div class="logo-container"><img src="/logo.png" alt="" /></div>
+      <div class="background"><img src="/photp.png" alt="" /></div>
       <nav class="menu">
         <ul>
           <li>
@@ -42,70 +43,72 @@
         </div>
       </div>
     </div>
-    <div class="services1">
-      <h2>What I do</h2>
-      <img src="/mobile_web_design.png" alt="" />
-      <h4>Mobile & Web Design</h4>
-      <p>
-        Need a website or mobile app? I can create <br />
-        beautiful websites or mobile apps based on <br />
-        your content, or redesign your old website <br />
-        to improve user engagement or achieve <br />
-        your business goals.
-      </p>
-    </div>
+    <div class="flexbox-container">
+      <div class="flexbox-item-1">
+        <h2>What I do</h2>
+        <img src="/mobile_web_design.png" alt="" />
+        <h4>Mobile & Web Design</h4>
+        <p>
+          Need a website or mobile app? I can create <br />
+          beautiful websites or mobile apps based on <br />
+          your content, or redesign your old website <br />
+          to improve user engagement or achieve <br />
+          your business goals.
+        </p>
+      </div>
 
-    <div class="services2">
-      <svg
-        width="73"
-        height="55"
-        viewBox="0 0 73 55"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M9.75107 25.7976L12.8781 10.1611C13.2363 8.37116 15.5495 7.84647 16.6455 9.30631L25.908 21.6564C27.3777 23.617 25.5597 24.074 19.4664 31.589C11.3929 27.6298 9.22919 28.4069 9.75107 25.7976Z"
-          fill="#336699"
-        />
-        <path
-          d="M52.8906 31.589C46.7635 24.2348 44.7987 23.6677 46.3079 21.6564L55.5705 9.30631C56.665 7.84647 58.9796 8.37116 59.3378 10.1611L62.4649 25.7976C63.0079 28.5184 60.3929 27.83 52.8906 31.589Z"
-          fill="#336699"
-        />
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M53.6839 28.6086L68.8338 17.3404C70.4262 16.157 72.6364 17.6323 72.1484 19.559L66.3655 42.3072L63.6094 44.4229L65.2907 46.5386L63.5445 53.4062C63.3062 54.3427 62.4613 55 61.4937 55H10.7223C9.75471 55 8.90984 54.3427 8.67147 53.4062L6.9253 46.5386L9.67996 44.6119L5.85193 42.3086L0.0689913 19.5604C-0.419032 17.6366 1.78695 16.1542 3.38219 17.3404L18.5307 28.6086L34.2716 1.06491C34.6778 0.355439 35.3943 0 36.1094 0C36.8259 0 37.5396 0.355439 37.9444 1.06491L53.6839 28.6086ZM37.9212 22.1797L44.2669 32.7569C44.6689 33.4268 44.6689 34.2647 44.2669 34.9346L37.9212 45.5118C37.5121 46.193 36.8097 46.5372 36.1087 46.5386C35.4077 46.5386 34.7053 46.1973 34.2934 45.5118L27.9477 34.9346C27.5457 34.2647 27.5457 33.4268 27.9477 32.7569L34.2934 22.1797C34.7025 21.497 35.4063 21.1529 36.1087 21.1529C36.8097 21.1529 37.5093 21.4942 37.9212 22.1797Z"
-          fill="#FF6666"
-        />
-        <path
-          d="M21.2988 44.4229C21.2988 45.5907 20.3509 46.5386 19.1831 46.5386H6.92463L5.84985 42.3072H19.1831C20.3509 42.3072 21.2988 43.255 21.2988 44.4229Z"
-          fill="#336699"
-        />
-        <path
-          d="M66.3635 42.3072L65.2887 46.5386H53.0303C51.8624 46.5386 50.9146 45.5907 50.9146 44.4229C50.9146 43.255 51.8624 42.3072 53.0303 42.3072H66.3635Z"
-          fill="#336699"
-        />
-      </svg>
+      <div class="flexbox-item-2">
+        <svg
+          width="73"
+          height="55"
+          viewBox="0 0 73 55"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9.75107 25.7976L12.8781 10.1611C13.2363 8.37116 15.5495 7.84647 16.6455 9.30631L25.908 21.6564C27.3777 23.617 25.5597 24.074 19.4664 31.589C11.3929 27.6298 9.22919 28.4069 9.75107 25.7976Z"
+            fill="#336699"
+          />
+          <path
+            d="M52.8906 31.589C46.7635 24.2348 44.7987 23.6677 46.3079 21.6564L55.5705 9.30631C56.665 7.84647 58.9796 8.37116 59.3378 10.1611L62.4649 25.7976C63.0079 28.5184 60.3929 27.83 52.8906 31.589Z"
+            fill="#336699"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M53.6839 28.6086L68.8338 17.3404C70.4262 16.157 72.6364 17.6323 72.1484 19.559L66.3655 42.3072L63.6094 44.4229L65.2907 46.5386L63.5445 53.4062C63.3062 54.3427 62.4613 55 61.4937 55H10.7223C9.75471 55 8.90984 54.3427 8.67147 53.4062L6.9253 46.5386L9.67996 44.6119L5.85193 42.3086L0.0689913 19.5604C-0.419032 17.6366 1.78695 16.1542 3.38219 17.3404L18.5307 28.6086L34.2716 1.06491C34.6778 0.355439 35.3943 0 36.1094 0C36.8259 0 37.5396 0.355439 37.9444 1.06491L53.6839 28.6086ZM37.9212 22.1797L44.2669 32.7569C44.6689 33.4268 44.6689 34.2647 44.2669 34.9346L37.9212 45.5118C37.5121 46.193 36.8097 46.5372 36.1087 46.5386C35.4077 46.5386 34.7053 46.1973 34.2934 45.5118L27.9477 34.9346C27.5457 34.2647 27.5457 33.4268 27.9477 32.7569L34.2934 22.1797C34.7025 21.497 35.4063 21.1529 36.1087 21.1529C36.8097 21.1529 37.5093 21.4942 37.9212 22.1797Z"
+            fill="#FF6666"
+          />
+          <path
+            d="M21.2988 44.4229C21.2988 45.5907 20.3509 46.5386 19.1831 46.5386H6.92463L5.84985 42.3072H19.1831C20.3509 42.3072 21.2988 43.255 21.2988 44.4229Z"
+            fill="#336699"
+          />
+          <path
+            d="M66.3635 42.3072L65.2887 46.5386H53.0303C51.8624 46.5386 50.9146 45.5907 50.9146 44.4229C50.9146 43.255 51.8624 42.3072 53.0303 42.3072H66.3635Z"
+            fill="#336699"
+          />
+        </svg>
 
-      <h4>Graphic Design</h4>
-      <p>
-        Need art for your project? I can create <br />
-        beautiful art, designs based on your project <br />
-        requirements such as business cards, digital <br />
-        banners, icons, logos, hand-drawn <br />
-        characters, abstracts, textures, etc.
-      </p>
-    </div>
-    <div class="services3">
-      <img src="frontend.png" alt="" />
-      <h4>Graphic Design</h4>
-      <p>
-        Already have the design? I can create static <br />
-        websites such as landing pages, company <br />
-        sites, and custom websites with minimal <br />
-        javascript. Will deliver a well-documented <br />
-        code.
-      </p>
+        <h4>Graphic Design</h4>
+        <p>
+          Need art for your project? I can create <br />
+          beautiful art, designs based on your project <br />
+          requirements such as business cards, digital <br />
+          banners, icons, logos, hand-drawn <br />
+          characters, abstracts, textures, etc.
+        </p>
+      </div>
+      <div class="flexbox-item-3">
+        <img src="frontend.png" alt="" />
+        <h4>Frontend development</h4>
+        <p>
+          Already have the design? I can create static <br />
+          websites such as landing pages, company <br />
+          sites, and custom websites with minimal <br />
+          javascript. Will deliver a well-documented <br />
+          code.
+        </p>
+      </div>
     </div>
     <div class="clients"></div>
   </div>
@@ -158,13 +161,16 @@ li {
   font-size: 20px;
   line-height: 22px;
 }
+.background {
+  display: inline-block;
+}
 .button1 {
   background-color: #336699;
   display: inline-flex;
-  padding: 10px 26px;
+  padding: 10px 30px;
   border: none;
   font-size: 20px;
-  font-weight: 400;
+  font-weight: 500;
   font-family: 'Roboto';
   font-style: normal;
   color: #ffffff;
@@ -177,16 +183,25 @@ li {
 .button2 {
   background-color: var(--color-primary);
   display: inline-flex;
-  padding: 10px 26px;
+  padding: 10px 27px;
   border: none;
   font-size: 20px;
   font-weight: 600;
   font-family: 'Roboto';
   font-style: normal;
   border-radius: 5px;
-  margin-left: 16px;
+  margin-left: 20px;
   margin-top: 50px;
 }
+.flexbox-container {
+  display: inline-flex;
+  top: 700px;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 400;
+}
+
+/*
 .services1 {
   position: absolute;
   top: 700px;
@@ -196,6 +211,7 @@ li {
   font-size: 20px;
   color: #a4a4a4;
 }
+*/
 h2 {
   color: #ffffff;
   margin-bottom: 50px;
@@ -205,6 +221,7 @@ h4 {
   margin-bottom: 25px;
   margin-top: 15px;
 }
+/*
 .services2 {
   top: 780px;
   position: absolute;
@@ -223,6 +240,7 @@ h4 {
   font-size: 20px;
   color: #a4a4a4;
 }
+*/
 
 .nav-text {
   color: var(--color-primary);
